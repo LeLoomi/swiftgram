3aa4d8a4b53d7c1dabb342de6d333e3cc70ef99d
Eliah Lohr <eliah.lohr@mailbox.tu-dresden.de>
2022-09-21 19:59:15 +0200

    added icons, views setup

---
+++ swiftgram/swiftgram/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftgram
+//
+//  Created by Eliah Lohr on 21.09.22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swiftgram/swiftgram/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftgram
+//
+//  Created by Eliah Lohr on 21.09.22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
