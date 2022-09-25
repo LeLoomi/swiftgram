5248a768654db36e57518bf7e964d9471bda7d3b
Eliah Lohr <eliah.lohr@mailbox.tu-dresden.de>
2022-09-25 15:26:02 +0200

    swiftui project setup

---
+++ Swiftgram/Swiftgram/SwiftgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftgramApp.swift
+//  Swiftgram
+//
+//  Created by Eliah Lohr on 25.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
